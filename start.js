e8965029c617afe9d07d221bd4ccc3100a905a65
cwache <carsten.wache@googlemail.com>
2018-06-13 23:25:27 +0200

    add windows title and set min dimentions

--- start.js
+++ start.js
@@ -1,7 +1,12 @@
+var pkg = require('./package.json');
+
 nw.Window.open('./main.html', {
     'id': 'kissgui',
-    'height': 680,
-    'width': 920,
+    'title': 'KISS-GUI ' + pkg.version,
+    'height': 600,
+    'width': 970,
+    "min_width": 900,
+    "min_height": 600,
     'position': 'center',
     'resizable': true
 }, function(c){
