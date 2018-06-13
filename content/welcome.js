b7bc9fd9feca4fe433c297c149cc97cf3a266f83
cwache <carsten.wache@googlemail.com>
2018-06-14 00:24:54 +0200

    update changelog + wip remove hardcoded version number

--- content/welcome.js
+++ content/welcome.js
@@ -41,6 +41,7 @@ CONTENT.welcome.initialize = function (callback) {
         $("#portArea").show();
         $('#menu').show();
         $(".navigation-menu-button").css("display", "");
+        $('#gui_version').text(" v2.0.7");
 
         $("#language").on("change", function () {
             var lang = $(this).val();
