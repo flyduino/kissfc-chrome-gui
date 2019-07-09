2f6f9e52ca38ee565133344adf1988f9c9e67bf8
Carsten Wache <carsten.wache@googlemail.com>
2019-07-09 17:45:26 +0200

    fix crash on mac when close app

--- start.js
+++ start.js
@@ -9,10 +9,4 @@ nw.Window.open('./main.html', {
     "min_height": 600,
     'position': 'center',
     'resizable': true
-}, function(c){
-    nw.global.mainWin = c;
-    c.on("close", function(){
-        nw.App.closeAllWindows();
-        c.close(true);
-    });
 });
