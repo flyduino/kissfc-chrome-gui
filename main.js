95c69303bda244201bd778b3aa7a97b8e7cd0986
Carsten W <lichtl@users.noreply.github.com>
2021-06-22 22:09:49 +0200

    M1  (#108)
    *M1 fix
    *fix on lang files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const MIN_CONFIG_VERSION = 108; // this gui can manage versions in this range
-const MAX_CONFIG_VERSION = 128;
+const MAX_CONFIG_VERSION = 129; //128 ghost // 129 = bec support
 
 function getLanguage(callback) {
     if (typeof chromeSerial !== 'undefined') {
